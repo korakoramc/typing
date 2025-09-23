8dc6d851f7148523f53cac14afa32d0077183b7a
korakoramc <kora0502mc@gmail.com>
2025-09-23 16:05:54 +0900

    add Sentences.java

---
+++ src/main/java/com/github/korakoramc/Sentences.java
@@ -0,0 +1,18 @@
+package com.github.korakoramc;
+
+public class Sentences {
+    String[][] sentences={
+        {"私はJavaを勉強中です","わたしはJavaをべんきょうちゅうです。"},
+        {"私の電話番号は123-456-789です。","わたしのでんわばんごうは123-456-789です。"},
+        {"今日は12月13日です。","きょうは12がつ13にちです"}
+    };
+
+    public String getMainSentence(int n){
+        return sentences[n][0];
+    }
+    
+    public String getSubSentence(int n) {
+        return sentences[n][1];
+    }
+}
+
